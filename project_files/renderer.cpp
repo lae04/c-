1b6bfa56412bff6504d428ef7afce0b28b58130e
Haong <parkhy050509@gmail.com>
2026-05-27 17:22:31 +0900

    Update renderer.cpp

--- project_files/renderer.cpp
+++ project_files/renderer.cpp
@@ -39,9 +39,9 @@ void quitRenderer() {
 void renderScoreboard(const ScoreData& score) {
     if (!g_scoreWin) return;
 
-    touchwin(g_scoreWin); //수정_하윤 추가
+    //touchwin(g_scoreWin); //수정_하윤 추가
     
-    //werase(g_scoreWin);
+    werase(g_scoreWin);
     box(g_scoreWin, 0, 0);
 
     mvwprintw(g_scoreWin, 1, 2, "=== Score Board ===");
